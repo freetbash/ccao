2640c8f3f104a7d84979553f35903491f041a497
Bash <bash@freet.tech>
2022-04-16 14:08:25 +0800

    new mode

--- build.sh
+++ build.sh
@@ -10,9 +10,6 @@ g++ \
    -I./apps/classes/headers \
    -I./apps/public/headers \
    -I./apps/tools/headers \
-   -I/home/bash/.ccao/stars/toml/0 \
    -I./ccao/headers \
-   -L/home/bash/.ccao/stars/toml/0 \
-   -ltoml \
    -o ./bin/ccao -static -std=c++11
 
