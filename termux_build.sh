ffed367ba6f5345571fcafac1acec98e9de1afe0
Bash <bash@freet.tech>
2022-04-16 14:28:45 +0800

    sh

--- termux_build.sh
+++ termux_build.sh
@@ -2,7 +2,7 @@
 
 
 
-g++ \
+aarch64-linux-gnu-g++ \
   apps/classes/source/*.cpp \
   apps/public/source/*.cpp \
   apps/tools/source/*.cpp \
@@ -13,5 +13,5 @@ g++ \
    -I./apps/tools/headers \
    -I./toml/headers \
    -I./ccao/headers \
-   -o ./bin/ccao -static -std=c++11
+   -o ./bin/ccao_for_termux -static -std=c++11
 
